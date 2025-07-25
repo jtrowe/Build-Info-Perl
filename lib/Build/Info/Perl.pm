c5533b32974c8b596117ccdb877c7f60ccee1d8c
Joshua T. Rowe <jrowe@jrowe.org>
2025-07-25 14:28:40 -0400

    Add generate_build_info sub
    Update version to 0.1.1.
    
    Ticket: #3 Add logic to generate a build info file with only version information

--- lib/Build/Info/Perl.pm
+++ lib/Build/Info/Perl.pm
@@ -4,9 +4,259 @@ use 5.030003;
 use strict;
 use warnings;
 
-our @ISA = qw();
+use Exporter qw( import );
 
-our $VERSION = '0.1.0';
+our @EXPORT_OK = qw(
+    $VERSION
+
+    generate_build_info
+    generate_build_info_exports
+    generate_build_info_footer
+    generate_build_info_header
+    generate_build_info_var
+    generate_build_info_vars
+);
+
+our $VERSION = '0.1.1';
+
+
+my %DESC = (
+    VERSION => 'The version of the module.',
+);
+
+
+sub generate_build_info {
+    my %param   = @_;
+    my $desc    = $param{desc}    // \%DESC,
+    my $env     = $param{env}     // \%ENV,
+    my $module  = $param{module}  // die 'parameter module is undef';
+    my $out     = $param{out}     // die 'parameter out is undef';
+    my $package = $param{package} // die 'parameter package is undef';
+    my $tags    = $param{tags};
+    my $vars    = $param{vars};
+
+    generate_build_info_header(
+        module  => $module,
+        out     => $out,
+        package => $package,
+    );
+
+    my $vars_from_tags = generate_build_info_exports(
+        out  => $out,
+        tags => $tags,
+        vars => $vars,
+    );
+
+    unless ( $vars ) {
+        $vars = $vars_from_tags;
+    }
+
+    generate_build_info_vars(
+        desc => $desc,
+        env  => $env,
+        out  => $out,
+        tags => $tags,
+        vars => $vars,
+    );
+
+    generate_build_info_footer(
+        out => $out,
+    );
+
+
+    return;
+}
+
+
+sub generate_build_info_exports {
+    my %param = @_;
+    my $out   = $param{out}  // die 'parameter out is undef';
+    my $tags  = $param{tags} // die 'parameter tags is undef';
+    my @vars  = @{ $param{vars} // [] };
+
+    # NB: Param vars will be used for 'all' %EXPORT_TAG if available.
+    #     Otherwise, 'all' will be all unique values from all the other
+    #     tags.
+
+    my @all;
+    my %all;
+
+    my @tags = sort keys %{ $tags };
+
+    print $out join("\n",
+        '',
+        'our %EXPORT_TAGS = (',
+        '',
+    );
+
+    foreach my $tag ( @tags, 'all' ) {
+        my @items = @{ $tags->{$tag} // [] };
+
+        my $is_all = 'all' eq $tag;
+        if ( $is_all ) {
+            # NB: Can pass in vars param that will override the all tag
+            if ( @vars ) {
+                @items = @vars;
+            }
+            else {
+                @items = sort keys %all;
+                @all = @items;
+            }
+        }
+
+        if ( @items ) {
+            print $out join("\n",
+                sprintf('    %s => [ qw(', $tag),
+                '',
+            );
+
+            foreach my $item ( @items ) {
+                unless ( $is_all ) {
+                    $all{$item} = 1;
+                }
+
+                print $out join("\n",
+                    sprintf('        $%s', $item),
+                    '',
+                );
+            }
+
+            print $out join("\n",
+                '    ),',
+                '',
+            );
+
+        }
+    }
+
+    print $out join("\n",
+        ');',
+        '',
+    );
+
+    print $out join("\n",
+        '',
+        'our @EXPORT_OK = qw(',
+        '',
+    );
+
+    foreach my $item ( sort( keys(%all), @vars ) ) {
+        print $out join("\n",
+            sprintf('    $%s', $item),
+            '',
+        );
+    }
+
+    print $out join("\n",
+        ');',
+        '',
+    );
+
+    return \@all;;
+}
+
+
+sub generate_build_info_footer {
+    my %param = @_;
+    my $out   = $param{out} // die 'parameter out is undef';
+
+    print $out join("\n",
+        '',
+        '1;',
+        '',
+    );
+
+    return;
+}
+
+
+sub generate_build_info_header {
+    my %param   = @_;
+    my $module  = $param{module}  // die 'parameter module is undef';
+    my $out     = $param{out}     // die 'parameter out is undef';
+    my $package = $param{package} // die 'parameter package is undef';
+
+    print $out join("\n",
+        sprintf('package %s;', $package),
+                '',
+                'use 5.030003',
+                'use strict;',
+                'use warnings;',
+                '',
+                '',
+                'use Exporter qw( import );',
+                '',
+                '=head1 NAME',
+                '',
+        sprintf('%s - Build information for package %s', $package, $module),
+                '',
+                '=cut',
+                '',
+    );
+
+    return;
+}
+
+
+sub generate_build_info_var {
+    my %param = @_;
+    my $desc  = $param{desc} // '';
+    my $out   = $param{out}  // die 'parameter out is undef';
+    my $val   = $param{val}  // die 'parameter val is undef';
+    my $var   = $param{var}  // die 'parameter var is undef';
+
+    my @desc;
+    if ( $desc ) {
+        @desc = (
+            sprintf('%s', $desc),
+                    '',
+        );
+    }
+
+    print $out join("\n",
+                '',
+        sprintf('=head2 $%s', $var),
+                '',
+        sprintf('%s', $val),
+                '',
+        @desc,
+                '=cut',
+                '',
+        sprintf('our $%s = "%s";', $var, $val),
+                '',
+    );
+
+    return;
+}
+
+
+sub generate_build_info_vars {
+    my %param = @_;
+    my $desc  = $param{desc} // {};
+    my $env   = $param{env}  // die 'parameter env is undef';
+    my $out   = $param{out}  // die 'parameter out is undef';
+    my $vars  = $param{vars} // die 'parameter vars is undef';
+
+    print $out join("\n",
+        '',
+        '=head1 VARIABLES',
+        '',
+        'All of the variables available for export.',
+        '',
+        '=cut',
+        '',
+    );
+    foreach my $var ( @{ $vars } ) {
+        generate_build_info_var(
+            desc => $desc->{$var},
+            out  => $out,
+            val  => $env->{$var},
+            var  => $var,
+        );
+    }
+
+    return;
+}
 
 
 1;
@@ -21,7 +271,7 @@ Joshua T. Rowe, E<lt>jrowe@home.jrowe.orgE<gt>
 
 =head1 VERSION
 
-0.1.0
+0.1.1
 
 =head1 COPYRIGHT AND LICENSE
 
--- t/Build-Info-Perl.t
+++ t/Build-Info-Perl.t
@@ -1,9 +1,190 @@
 use strict;
 use warnings;
 
-use Test::More tests => 1;
+use Test::More tests => 8;
 
 BEGIN {
-    use_ok('Build::Info::Perl');
+    my @exports = qw(
+        generate_build_info
+        generate_build_info_exports
+        generate_build_info_footer
+        generate_build_info_header
+        generate_build_info_var
+        generate_build_info_vars
+    );
+
+    use_ok('Build::Info::Perl' => @exports );
 };
+
+
+subtest "generate_build_info_exports" => sub {
+    plan(tests => 1);
+
+    my $buffer;
+    open(my $out, '>', \$buffer);
+
+    my %tags = (
+        ci => [ qw(
+            CI
+            CI_COMMMIT_SHA
+            GITLAB_CI
+        ) ],
+        gitlab => [ qw(
+            GITLAB_CI
+        ) ],
+    );
+
+    my $vars = [ qw(
+        SHELL
+    ) ];
+
+    generate_build_info_exports(
+        out  => $out,
+        tags => \%tags,
+        vars => $vars,
+    );
+
+    close $out;
+
+    ok(length($buffer), 'Generated some text')
+            or note("out.length => " . length($buffer));
+    note("out:\n" . $buffer);
+
+};
+
+
+subtest "generate_build_info_footer" => sub {
+    plan(tests => 1);
+
+    my $buffer;
+    open(my $out, '>', \$buffer);
+
+    generate_build_info_footer(
+        out => $out,
+    );
+
+    close $out;
+
+    ok(length($buffer), 'Generated some text')
+            or note("out.length => " . length($buffer));
+    note("out:\n" . $buffer);
+
+};
+
+
+subtest "generate_build_info_header" => sub {
+    plan(tests => 1);
+
+    my $buffer;
+    open(my $out, '>', \$buffer);
+
+    generate_build_info_header(
+        module  => 'Foo',
+        out     => $out,
+        package => 'Foo::Build::Info',
+    );
+
+    close $out;
+
+    ok(length($buffer), 'Generated some text')
+            or note("out.length => " . length($buffer));
+    note("out:\n" . $buffer);
+
+};
+
+
+subtest "generate_build_info_var" => sub {
+    plan(tests => 1);
+
+    my $buffer;
+    open(my $out, '>', \$buffer);
+
+    generate_build_info_var(
+        out => $out,
+        val => '1',
+        var => 'ALFA',
+    );
+
+    close $out;
+
+    ok(length($buffer), 'Generated some text')
+            or note("out.length => " . length($buffer));
+    note("out:\n" . $buffer);
+
+};
+
+
+subtest "generate_build_info_vars" => sub {
+    plan(tests => 1);
+
+    my $buffer;
+    open(my $out, '>', \$buffer);
+
+    my %env = (
+        VERSION => '1.2.3',
+    );
+
+    my @vars = qw(
+        VERSION
+    );
+
+    generate_build_info_vars(
+        env  => \%env,
+        out  => $out,
+        vars => \@vars,
+    );
+
+    close $out;
+
+    ok(length($buffer), 'Generated some text')
+            or note("out.length => " . length($buffer));
+    note("out:\n" . $buffer);
+
+};
+
+
+subtest "generate_build_info" => sub {
+    plan(tests => 1);
+
+    my $buffer;
+    open(my $out, '>', \$buffer);
+
+    my %env = (
+        CI      => 'true',
+        VERSION => '1.2.3',
+    );
+
+    my %tags = (
+        ci => [ qw(
+            CI
+        ) ],
+        version => [ qw(
+            VERSION
+        ) ],
+    );
+
+    generate_build_info(
+        env     => \%env,
+        module  => 'Bar',
+        out     => $out,
+        package => 'Bar::BuildInfo',
+        tags    => \%tags,
+    );
+
+    close $out;
+
+    ok(length($buffer), 'Generated some text')
+            or note("out.length => " . length($buffer));
+    note("out:\n" . $buffer);
+
+};
+
+
+subtest '$VERSION' => sub {
+
+    my $v = $Build::Info::Perl::VERSION;
+    like($v, qr/^\d+\.\d+\.\d+$/, '$VERSION looks ok');
+
+};
+
 
