c362480ca58569112d40c1fb987d3afb237185a9
Joshua T. Rowe <jrowe@jrowe.org>
2025-07-25 14:05:30 -0400

    Add dist files
    Ticket: #2 Add dist files

---
+++ lib/Build/Info/Perl.pm
@@ -0,0 +1,35 @@
+package Build::Info::Perl;
+
+use 5.030003;
+use strict;
+use warnings;
+
+our @ISA = qw();
+
+our $VERSION = '0.1.0';
+
+
+1;
+__END__
+=head1 NAME
+
+Build::Info::Perl - Collects build information into a Perl package.
+
+=head1 AUTHOR
+
+Joshua T. Rowe, E<lt>jrowe@home.jrowe.orgE<gt>
+
+=head1 VERSION
+
+0.1.0
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2025 by Joshua T. Rowe
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.30.3 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
